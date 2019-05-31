536bb1219e9e89f2caee7422c4f09dd2fde2b84a
codepixar <akash@spondonit.com>
2019-05-31 23:49:58 +0600

    Bug fixed

--- templates/header-nav.php
+++ templates/header-nav.php
@@ -1,54 +1,64 @@
 <header id="header">
-<div class="container header-top">
-    <div class="row">
-        <?php 
-        // Social Media
-        $offset = ' offset-md-6';
-        if( security_opt( 'security-headersocial-toggle-settings' ) ) {
-            $offset = '';
 
-            echo '<div class="col-6 top-head-left">';
-                if( has_nav_menu('social-menu') ) {
-                    $args = array(
-                        'theme_location' => 'social-menu',
-                        'container'      => '',
-                        'menu_class'     => '',
-                        'depth'          => 1,
-                        'fallback_cb'    => 'security_social_navwalker::fallback',
-                        'walker'         => new security_social_navwalker(),
-                    );  
-                    wp_nav_menu( $args );
-                }
-            echo '</div>';
-        }
+    <?php
+    $socialToggle = security_opt( 'security-headersocial-toggle-settings' );
+    $number     = security_opt( 'security_headtop_number' );
+    $bntText1   = security_opt( 'security_headtop_btn1_text' );
+    $bntText2   = security_opt( 'security_headtop_btn2_text' );
 
+    if( !empty( $socialToggle ) || !empty( $number ) || !empty( $bntText1 ) || !empty( $bntText2 ) ){
         ?>
-        <div class="col-6<?php echo esc_attr( $offset ); ?> top-head-right">
-            <ul>
+        <div class="container header-top">
+            <div class="row">
                 <?php
-                // Phone number
-                if( security_opt( 'security_headtop_number' ) ) {
+                // Social Media
+                $offset = ' offset-md-6';
+                if( $socialToggle  ) {
+                    $offset = '';
 
-                    $number = security_opt( 'security_headtop_number' );
+                    echo '<div class="col-6 top-head-left">';
+                        if( has_nav_menu('social-menu') ) {
+                            $args = array(
+                                'theme_location' => 'social-menu',
+                                'container'      => '',
+                                'menu_class'     => '',
+                                'depth'          => 1,
+                                'fallback_cb'    => 'security_social_navwalker::fallback',
+                                'walker'         => new security_social_navwalker(),
+                            );
+                            wp_nav_menu( $args );
+                        }
+                    echo '</div>';
+                }
 
-                    $telNumber = str_replace(' ', '', $number );
-
-                    echo '<li><a href="tel:' . esc_attr( $telNumber ) . '"><span>' . esc_html( $number ) . '</span></a></li>';
-                }
-                // Button #1
-                if( security_opt( 'security_headtop_btn1_text' ) && security_opt( 'security_headtop_btn1_url' ) ) {
-                    echo '<li><a href="'.esc_url( security_opt('security_headtop_btn1_url') ).'">'.esc_html( security_opt('security_headtop_btn1_text') ).'</a></li>';
-                }
-                // Button #2
-                if( security_opt( 'security_headtop_btn2_text' ) && security_opt( 'security_headtop_btn2_url' ) ) {
-                    echo '<li><a href="'.esc_url( security_opt( 'security_headtop_btn2_url' ) ).'">'.esc_html( security_opt( 'security_headtop_btn2_text' ) ).'</a></li>';
-                }
                 ?>
-            </ul>
-        </div>                 
-    </div>
-</div>
-<hr>
+                <div class="col-6<?php echo esc_attr( $offset ); ?> top-head-right">
+                    <ul>
+                        <?php
+                        // Phone number
+
+                        if( $number ) {
+
+                            $telNumber = str_replace(' ', '', $number );
+
+                            echo '<li><a href="tel:' . esc_attr( $telNumber ) . '"><span>' . esc_html( $number ) . '</span></a></li>';
+                        }
+                        // Button #1
+                        if( $bntText1 && security_opt( 'security_headtop_btn1_url' ) ) {
+                            echo '<li><a href="'.esc_url( security_opt('security_headtop_btn1_url') ).'">'.esc_html( security_opt('security_headtop_btn1_text') ).'</a></li>';
+                        }
+                        // Button #2
+                        if( $bntText2 && security_opt( 'security_headtop_btn2_url' ) ) {
+                            echo '<li><a href="'.esc_url( security_opt( 'security_headtop_btn2_url' ) ).'">'.esc_html( security_opt( 'security_headtop_btn2_text' ) ).'</a></li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <?php
+    } ?>
 <div class="container">
     <div class="row align-items-center justify-content-between d-flex">
         <?php 
